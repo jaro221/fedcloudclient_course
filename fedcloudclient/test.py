efe4149baab69b7a4cdcabd00b8c7305820a40f5
Jaromir_SAS <jaro15926@gmail.com>
2024-09-12 08:34:42 +0200

    initial commit

--- fedcloudclient/test.py
+++ fedcloudclient/test.py
@@ -2,5 +2,10 @@
 print(f"Ide to test #2")
 test_val=True
 if test_val:
+<<<<<<< HEAD
     print(f"{test_val} test if ")
 print(f"We are starting")
+=======
+    print(f"{test_val} test if ###3 {test_val}")
+print(f"We are starting")   
+>>>>>>> 78cf805 (initial commit)
